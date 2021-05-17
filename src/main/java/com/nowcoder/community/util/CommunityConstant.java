4fd45907acd92a474d9f8f03042bd90cc5259b6f
qingwang <827900939@qq.com>
2021-05-17 22:48:41 +0800

    2.3注册功能完成

---
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -0,0 +1,24 @@
+package com.nowcoder.community.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: qingwang
+ * @Date: 2021/05/17/22:00
+ * @Description:
+ */
+public interface CommunityConstant {
+    /**
+     * 激活成功
+     */
+    int ACTIVATION_SUCCESS = 0;
+    /**
+     * 重复激活
+     */
+    int ACTIVATION_REPEAT = 1;
+    /**
+     * 激活失败
+     */
+    int ACTIVATION_FAILURE = 2;
+
+}
--- src/main/java/com/nowcoder/community/util/CommunityUtil.java
+++ src/main/java/com/nowcoder/community/util/CommunityUtil.java
@@ -1,5 +1,11 @@
 package com.nowcoder.community.util;
 
+
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.DigestUtils;
+
+
 import java.util.UUID;
 
 /**
@@ -14,6 +20,14 @@ public class CommunityUtil {
 
     public static String generateUUID(){
         return UUID.randomUUID().toString().replaceAll("-","");
+    }
+
+
+    public static String md5(String key){
+        if(StringUtils.isBlank(key)){
+            return null;
+        }
+        return DigestUtils.md5DigestAsHex(key.getBytes());
     }
 
 }
